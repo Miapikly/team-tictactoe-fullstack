b0ff4da19f452e95f50031aaa91ac8eb1777f238
Nicolas Okuly <nicolasokuly@outlook.com>
2025-12-15 16:49:00 -0700

    Indentation changes

--- client/src/components/Game/Board.jsx
+++ client/src/components/Game/Board.jsx
@@ -2,19 +2,19 @@ import Cell from "./Cell.jsx";
 //import styles from "./cell.css";
 
 function Board({ board, onCellClick, winningCombo }) {
-  return (
-    <div className="board">
-      {board.map((value, index) => (
-        <Cell
-          value={value}
-          position={index}
-          key={index}
-          onClick={() => onCellClick(index)}
-          isWinning={winningCombo?.includes(index)}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="board">
+            {board.map((value, index) => (
+                <Cell
+                    value={value}
+                    position={index}
+                    key={index}
+                    onClick={() => onCellClick(index)}
+                    isWinning={winningCombo?.includes(index)}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default Board;
